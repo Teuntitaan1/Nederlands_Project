e0bc27f34ef629c37454eb7dbd601be5cd47b780
Teuntitaan1 <Teun.weijdener@gmail.com>
2023-08-29 13:20:42 +0200

    Initial functionality klaar

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [TextAreaValue, SetTextAreaValue] = useState("");
+
+  const [AllowedChars] = useState(Get_Allowed_Chars());
+  const [Prompt] = useState(Get_Prompt());
+
+  const [Done, SetDone] = useState(false);
+
+  function Get_Allowed_Chars() {
+    return [50, 100, 200, 500, 1000000][Math.round( 4 * Math.random())];
+  }
+  function Get_Prompt() {
+    return ["een nieuwsbericht over een gestorven eend"][Math.round( 0 * Math.random())];
+  }
+  
+  function Update_Text_Area(event) {
+    if (event.target.value.length < AllowedChars) {
+      SetTextAreaValue(event.target.value);
+    }
+  }
+  
+  function Send_Data() {
+    console.log(TextAreaValue);
+    SetDone(true);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      { !Done ?
+        <div>
+          <p>Schrijf een verhaaltje in {AllowedChars} tekens over {Prompt}</p>
+          <textarea value={TextAreaValue} onChange={(event) => {Update_Text_Area(event)}}></textarea>
+          <p>{AllowedChars - TextAreaValue.length - 1}</p>
+
+          <button onClick={() => {Send_Data()}}>Verstuur</button>
+        </div> :
+
+        <div>
+          <p>Bedankt voor uw tijd!</p>
+        </div>
+      }
     </>
-  )
+  );
 }
 
-export default App
+export default App;

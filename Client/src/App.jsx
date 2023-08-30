c7e6c4895e3762059c1fbd76bcc70d6176b0fba6
Teuntitaan1 <Teun.weijdener@gmail.com>
2023-08-30 13:52:09 +0200

    Lerp tussen kleuren

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css';
 import { useState} from 'react';
 
+function lerp( a, b, alpha ) {
+  return a + alpha * ( b - a );
+}
 
 function App() {
   const [TextAreaValue, SetTextAreaValue] = useState("");
@@ -9,7 +12,7 @@ function App() {
   const [Prompt] = useState(Get_Prompt());
 
   const [Done, SetDone] = useState(false);
-  const [Error, SetError] = useState(false);
+  const [HasError, SetHasError] = useState(false);
 
   function Get_Allowed_Chars() {
     return [50, 100, 200, 500, 1000000][Math.round( 4 * Math.random())];
@@ -31,7 +34,7 @@ function App() {
       AllowedChars : AllowedChars,
     }})
     .then(() => {SetDone(true);})
-    .catch(() => {SetError(true)});
+    .catch(() => {SetHasError(true)});
   }
 
   return (
@@ -40,10 +43,11 @@ function App() {
         <div>
           <p>Schrijf een verhaaltje in {AllowedChars} tekens over {Prompt}</p>
           <textarea value={TextAreaValue} onChange={(event) => {Update_Text_Area(event)}}></textarea>
-          <p>{AllowedChars - TextAreaValue.length}</p>
 
+          <p style={{color : `rgb(${lerp(0, 225, TextAreaValue.length/AllowedChars)}, ${lerp(0, 225, 1-(TextAreaValue.length/AllowedChars))}, 0)`}}>{AllowedChars - TextAreaValue.length}</p>
+          
           <button onClick={() => {Send_Data()}}>Verstuur</button>
-          {Error ? <p>Er is iets fout gegaan, probeer het nog eens!</p> : null}
+          {HasError ? <p>Er is iets fout gegaan, probeer het nog eens!</p> : null}
         </div> :
 
         <div>
